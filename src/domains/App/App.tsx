3beb8fc7a81f72c05942088445983da4b7cbe4fd
Noorodin Kiani <n.kiani@outlook.com>
2021-05-27 15:54:19 +0430

    initial commit.

---
+++ src/domains/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StyleWrapper from "./App.style";
+
+function App() {
+  return (
+    <StyleWrapper>
+      <div>Hello World!</div>
+    </StyleWrapper>
+  );
+}
+
+export default App;
